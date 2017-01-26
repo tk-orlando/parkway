ed76aa2876b4ff86ff97dbfea82910a0e08fa2b5
Michael Stevens <mike@eighty-8.com>
2017-01-25 23:11:37 -0500

    updates router for search

--- site/router.php
+++ site/router.php
@@ -35,6 +35,16 @@ class ParkwayRouter extends JComponentRouterBase
             $segments[] = $query['planid'];
             unset($query['planid']);
         };
+        if (isset($query['filter_property']))
+        {
+            $segments[] = $query['filter_property'];
+            unset($query['filter_property']);
+        };
+        if (isset($query['max']))
+        {
+            $segments[] = $query['max'];
+            unset($query['max']);
+        };
         return $segments;
 	}
 
@@ -54,6 +64,16 @@ class ParkwayRouter extends JComponentRouterBase
                 if(isset($segments[2])){ 
                     $vars['planid'] =$segments[2];
                 }
+                break;
+            case'buildings':
+                $vars['view'] = 'buildings';
+                $vars['layout'] =$segments[1];
+                if(isset($segments[2])){
+                    $vars['filter_property'] =$segments[2];
+                }
+                if(isset($segments[3])){
+                $vars['max'] =$segments[3];
+            }
                 break;
         }
         switch($segments[1])
